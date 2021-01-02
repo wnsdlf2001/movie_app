55e7be5e3b8dcd1649e5da574e914d7d1ed88501
Joon Il Yoon <wnsdlf2001@gmail.com>
2021-01-02 23:32:11 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food(dish){
+  return(
+    <h1>{dish.name} + {dish.picture}</h1>
+  )
+}
+
+const foodilke = [{id :1, name : "a", picture:"A", rating : 1.1}, {id :2, name : "b", picture:"B", rating : 1.1},{id :3, name : "c", picture:"C", rating : 1.1}];
+
+function renderFood(dish){
+  return <Food key={dish.name} name={dish.name} picture={dish.picture} />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      HELLO
+        {foodilke.map(renderFood)}
     </div>
+    
   );
 }
 
